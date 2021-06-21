905265640e1567a562c86f57d8c63ea418da9a0c
Luismatos19 <luismatosrobot@gmail.com>
2021-06-20 22:39:37 -0300

    setup inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <h1>dfadsfdsf</h1>
+      <Button text="Botão 1" />
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+type ButtonProps = {
+  text?: string;
+  children?: string;
+};
+
+export function Button(props: ButtonProps) {
+  return <button> {props.text} </button>;
+}
